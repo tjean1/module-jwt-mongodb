811abc489dbf71d3b898efc4c6004c40e8d77e25
tjean1 <th.jean59@gmail.com>
2022-04-04 21:41:51 +0200

    Update server.js with JWT

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 require('dotenv').config({ path: './config/.env' });
+require('./config/db');
+const { checkUser, requireAuth } = require('./middlewares/auth.middleware');
 
 const app = express();
 
@@ -9,6 +11,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// jwt
+app.get('*', checkUser);
+app.get('/jwtid', requireAuth, (req, res) => {
+    res.status(200).send(res.locals.user._id);
+});
 
 // server
 app.listen(process.env.PORT, () => {
